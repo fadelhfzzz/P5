320431a68f72180e935ae2a01edfe75d1a700ca4
fadelhafidz <fadellataa@gmail.com>
2023-11-11 16:25:38 +0700

    hasil update

--- FitNesseExpediter.java
+++ FitNesseExpediter.java
@@ -5,14 +5,15 @@ import java.net.Socket;
 import javax.print.attribute.standard.RequestingUserName;
 
 public class FitNesseExpediter implements ResponseSender {
-    private Socket          socket;
-    private InputStream     input;
-    private OutputStream    output;
-    private Request         request;
-    private Response        response;
+    private Socket socket;
+    private InputStream input;
+    private OutputStream output;
+    private Request request;
+    private Response response;
     private FitNesseContext context;
-    protected long          requestParsingTimeLimit;
-    private long            requestProgress;
-    private long            requestParsingDeadLine;
-    private boolean         hasError;
-}
+    protected long requestParsingTimeLimit;
+    private long requestProgress;
+    private long requestParsingDeadline;
+    private boolean hasError;
+    ...
+    }
